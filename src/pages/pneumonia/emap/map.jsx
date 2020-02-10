6091e88b6ee62c42862c1143b9c7e152c4bd29a6
zhanghao23 <386708307@qq.com>
2020-02-10 17:03:29 +0800

    省份详情

--- src/pages/pneumonia/emap/map.jsx
+++ src/pages/pneumonia/emap/map.jsx
@@ -1,26 +1,65 @@
 import { useState, useEffect } from '@tarojs/taro'
-import {getProvinces} from '../request.js'
+
+// import ReactEcharts from 'echarts-for-react/lib/core'
+// import echarts from 'echarts/lib/echarts'
+// import 'echarts/lib/chart/map'
+// import 'echarts/lib/component/visualMap'
+// import 'echarts/lib/component/tooltip'
+
+import {getProvinces, getChinaJson, getProvinceJson} from '../request.js'
 import { View, Text, Picker } from '@tarojs/components'
+import { 
+    AtList,
+    AtListItem,
+    AtModal, 
+    AtModalHeader, 
+    AtModalContent, 
+    AtModalAction
+} from "taro-ui"
 import './style.scss'
 import dayjs from 'dayjs'
 
 function EMap(props) {
-    const [provinceName, setProvinceName] = useState([])
-    const [provinces, setProvince] = useState([])
+    // 选择地区的下拉
+    const [provinceList, setProvinceList] = useState([])
+    // 省份列表数据
+    const [provinceDetail, setProvinceDetail] = useState([])
+    // 省份详情模态框
+    
+    const [isOpened, setModalIsOpend] = useState(false)
+    const [ProvinceDetailForModal, setProvinceDetailForModal] = useState({})
+
     const [selectorName, setSelectorName] = useState('')
+
     // const {desc: {confirmedCount}} = props
     useEffect(async() => {
         const {data: {code, newslist}}= await getProvinces()
-        const provinceName = newslist.map(i => i.provinceShortName)
+        const provinceList = newslist.map(i => i.provinceShortName)
         if(code === 200) {
-            setProvince(newslist)
-            setProvinceName(provinceName)
+            provinceList.unshift('全国')
+            setProvinceList(provinceList)
+            setProvinceDetail(newslist)
         }
+
+        // const xx = await getChinaJson()
+        // console.log(xx);
     }, [])
 
     const cityChange = (e) => {
         setSelectorName(provinces[e.detail.value]['provinceShortName'])
     }
+
+    const itemClickHandler = (locationId) => {
+        const detail = provinceDetail.filter(i => i.locationId === locationId)[0]
+        console.log(detail);
+        setModalIsOpend((pre) => !pre)
+        setProvinceDetailForModal(detail)
+    }
+
+    const modalClose = () => {
+        setModalIsOpend((pre) => !pre)
+    }
+
     return props.desc ? (
         <View className="emap">
             <Text className="title">全国截止{dayjs(props.desc.modifyTime).format('YYYY年MM月DD日 HH:ss')}(北京时间)统计</Text>
@@ -67,13 +106,47 @@ function EMap(props) {
                 </View>
             </View>
             <View>
-              <Picker mode='selector' range={provinceName} onChange={cityChange}>
+              {/* <Picker mode='selector' range={provinceList} onChange={cityChange}>
                 <View className='picker'>
                     <Text>地区: </Text>
                     <Text>{selectorName}</Text>
                 </View>
-              </Picker>
+              </Picker> */}
             </View>
+
+            <View>
+                <AtList>
+                    {provinceDetail.map(item => 
+                        <AtListItem 
+                            key={item.locationId}
+                            title={`${item.provinceShortName}`}
+                            note={`确诊：${item.confirmedCount} 死亡：${item.deadCount} 治愈：${item.curedCount}`} 
+                            arrow="right"
+                            onClick={() => itemClickHandler(item.locationId)} />
+                    )}
+                    
+                </AtList>
+            </View>
+
+            <AtModal isOpened={isOpened} closeOnClickOverlay onClose={modalClose}>
+                <AtModalHeader>{ProvinceDetailForModal.provinceName}</AtModalHeader>
+                <AtModalContent>
+                    <View className="header">
+                        <View className="item">城市</View>
+                        <View className="item">确诊</View>
+                        <View className="item">死亡</View>
+                        <View className="item">治愈</View>
+                    </View>
+                    {ProvinceDetailForModal.cities.map(item => 
+                        <View  className="header" key={item.locationId}>
+                            <View className="item">{item.cityName}</View>
+                            <View className="item">{item.confirmedCount}</View>
+                            <View className="item">{item.deadCount}</View>
+                            <View className="item">{item.curedCount}</View>
+                        </View>
+                    )}
+                </AtModalContent>
+            </AtModal>
         </View>
     ): null
 }
--- src/pages/pneumonia/request.js
+++ src/pages/pneumonia/request.js
@@ -12,8 +12,22 @@ const getProvinces = () => {
         url: 'https://api.tianapi.com/txapi/ncovcity/index?key=9a4ab8ec328de7fb7a675e4c2e936110',
     })
 }
+// 获取地图json文件
+const getChinaJson = () => {
+    return fly.get({
+        url: 'https://raw.githubusercontent.com/huanent/vue-echarts-map-demo/master/map/china.json',
+    })
+}
+// 获取地图json文件
+const getProvinceJson = (pinyinName) => {
+    return fly.get({
+        url: `https://raw.githubusercontent.com/huanent/vue-echarts-map-demo/master/map/province/${pinyinName}.json`,
+    })
+}
 
 export {
     getNews,
-    getProvinces
+    getProvinces,
+    getChinaJson,
+    getProvinceJson
 }
